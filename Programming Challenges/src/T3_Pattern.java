79eb6b8b11fbdf2930b8cd1c86bab26dc64c4cc7
Hamza Shahzad <hamza.shahzad.8692@gamail.com>
2024-03-16 15:08:17 +0500

    Rename classes T3_Pattern and T4_SingleLine

--- Programming Challenges/src/T3_Pattern.java
+++ Programming Challenges/src/T3_Pattern.java
@@ -1,4 +1,4 @@
-class Pattern {
+class T3_Pattern {
     public static void main(String[] args) {
         System.out.println("*");
         System.out.println("* *");
--- Programming Challenges/src/T4_SingleLine.java
+++ Programming Challenges/src/T4_SingleLine.java
@@ -1,4 +1,4 @@
-class SingleLine {
+class T4_SingleLine {
     public static void main(String[] args) {
         System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
         System.out.println("\n\n* * * * *\n* * * *\n* * *\n* *\n*");
