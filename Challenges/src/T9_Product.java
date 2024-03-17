da4fa7d5346e97acb14b3fdbbf944fff1b2563cd
Hamza Shahzad <hamza.shahzad.8692@gamail.com>
2024-03-17 11:38:20 +0500

    9 Task

---
+++ Challenges/src/T9_Product.java
@@ -0,0 +1,9 @@
+ class T9_Product {
+     public static void main(String[] args) {
+         float num1 = 10.111111f;
+         float num2 = 10.222222f;
+         float sum = num1 + num2;
+         System.out.println("sum of Float Number is: "+ sum);
+         // we can add maximum 6-7 digits after decimal in float
+     }
+}
